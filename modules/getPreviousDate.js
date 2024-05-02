602ffb23743c3af6fee180d299177ed551b1e493
Saheer <sshaik5@umassd.edu>
2024-05-02 15:18:17 -0400

    tests

---
+++ modules/getPreviousDate.js
@@ -0,0 +1,16 @@
+const lastDayOfMonth = require('./lastDayOfMonth');
+
+function getPreviousDate(month, day, year) {
+    const lastDay = lastDayOfMonth(month -1, year);
+    if (day > 1) {
+        return { month, day: day - 1, year };
+    } else {
+        if (month > 1) {
+            return { month: month - 1, day:lastDay, year };
+        } else {
+            return { month: 12, day: 31, year: year - 1 };
+        }
+    }
+}
+
+module.exports = getPreviousDate;
